5662aa95c23f3a1c2d7f30b0d9d84d48a1e09358
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-11-19 17:58:34 -0600

    Add models

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express =require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const blogRouter = require("./routes/blogRoutes")
 
 const app = express()
 app.use(cors)
 app.use(express.json)
+app.use(blogRouter)
 
 mongoose.connect('mongodb://localhost:27017/cindy')
     .then(() => console.log('Connected to MongoDB'))
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -2,3 +2,10 @@ const express = require('express')
 const router = express.Router()
 const blogController = require("../controllers/blogController")
 
+router.get("/", blogController.get_blogs)
+router.get("/getblog/:id", blogController.get_blog)
+router.post("/createblog", blogController.create_blog)
+router.put("/updateblog/:id", blogController.update_blog)
+router.delete("/deleteblog/:id", blogController.delete_blog)
+
+module.exports = router;
