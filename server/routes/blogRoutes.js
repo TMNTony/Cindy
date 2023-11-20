7c6ad7286e8bfd2b86d20ac7511db12e1a96df45
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-11-19 18:51:02 -0600

    Add routes

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const blogController = require("../controllers/blogController")
 
-router.get("/", blogController.get_blogs)
-router.get("/getblog/:id", blogController.get_blog)
-router.post("/createblog", blogController.create_blog)
-router.put("/updateblog/:id", blogController.update_blog)
-router.delete("/deleteblog/:id", blogController.delete_blog)
+router.get("/blogs", blogController.get_blogs)
+router.get("/blogs/:id", blogController.get_blog)
+router.post("/blogs/createBlog", blogController.create_blog)
+router.put("/blogs/updateBlog/:id", blogController.update_blog)
+router.delete("/blogs/deleteBlog/:id", blogController.delete_blog)
 
 module.exports = router;
---
+++ server/routes/videoRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+const videoController = require("../controllers/videoController")
+
+router.get("/videos", videoController.get_videos)
+router.post("videos/createVideo", videoController.create_video)
+router.put("videos/updateVideo/:id", videoController.update_video)
+router.delete("videos/deleteVideo/:id", videoController.delete_video)
+
+module.exports = router
